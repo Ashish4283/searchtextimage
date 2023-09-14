cbaafcd011ae792a43898af9b6ded30a9f002df0
Ashish B Jiwa <43324295+Ashish4283@users.noreply.github.com>
2023-09-14 06:53:00 +0530

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,11 +1,27 @@
 <?php
-if (isset($_GET['url'])) {
-    $url = urldecode($_GET['url']);
-    $image = file_get_contents($url);
-    header('Content-Type: image/jpeg'); // Change the content type based on the image format.
-    echo $image;
+// Get the URL of the image from the 'url' parameter
+$imageUrl = $_GET['url'];
+
+// Check if the URL is valid (you may want to add more robust validation)
+if (filter_var($imageUrl, FILTER_VALIDATE_URL)) {
+    // Fetch the image data
+    $imageData = file_get_contents($imageUrl);
+
+    // Check if the image data was successfully fetched
+    if ($imageData !== false) {
+        // Set the appropriate Content-Type header for the image
+        header('Content-Type: ' . mime_content_type($imageUrl));
+        
+        // Echo the image data
+        echo $imageData;
+    } else {
+        // Handle the case where image data couldn't be fetched
+        header('HTTP/1.1 500 Internal Server Error');
+        echo 'Error fetching image data.';
+    }
 } else {
+    // Handle invalid URL
     header('HTTP/1.1 400 Bad Request');
-    echo 'Invalid request.';
+    echo 'Invalid image URL.';
 }
 ?>
